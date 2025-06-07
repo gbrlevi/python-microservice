cfa639b1351e156445eadc61bacf46ea41622dde
Gabriel Levi <powerzinns@gmail.com>
2025-06-07 18:10:57 -0300

    cors update for vercel

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
-    allow_credentials=True,
+    allow_origins=[
+        "https://nutricao-frontend-final-k9n8.vercel.app",  # Replace with your real URL
+        "http://localhost:3000",  # For local development
+    ],
+    allow_credentials=True,  # Now you can use True
     allow_methods=["*"],
     allow_headers=["*"],
 )
